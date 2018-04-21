81465060305f433e7f00174c8fbb9c67506e89a3
Malegkin <malegkin@gmail.com>
2018-04-22 01:33:51 +0300

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,15 @@
 #include "foo.h"
 
 int main()
-{    
+{
+    cout << to_ip_string( char(-1)) << endl;
+    cout << to_ip_string( short(0)) << endl;
+    cout << to_ip_string( int(2130706433)) << endl;
+    cout << to_ip_string( long(8875824491850138409)) << endl;
+    cout << to_ip_string( string("8.8.8.8")) << endl;
+    cout << to_ip_string( vector<int>({1, 2, 3})) << endl;
+    cout << to_ip_string( list<int>({1, 2, 3, 4})) << endl;
+    cout << to_ip_string( make_tuple(1 , 2, 3, 4, 5 )) << endl;
 
     return 0;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -10,7 +10,14 @@ BOOST_AUTO_TEST_SUITE( test_suite_main )
 
 BOOST_AUTO_TEST_CASE( test_block_allocator )
 {
-//      BOOST_REQUIRE( pair.first  == pair.second);        
+    BOOST_REQUIRE( to_ip_string( char(-1)) == "255" );
+    BOOST_REQUIRE( to_ip_string( short(0)) == "0.0" );
+    BOOST_REQUIRE( to_ip_string( int(2130706433)) == "127.0.0.1" );
+    BOOST_REQUIRE( to_ip_string( long(8875824491850138409)) == "123.45.67.89.101.112.131.41" );
+    BOOST_REQUIRE( to_ip_string( "8.8.8.8") == "8.8.8.8" );
+    BOOST_REQUIRE( to_ip_string( vector<int>({1, 2, 3})) == "1..2..3" );
+    BOOST_REQUIRE( to_ip_string( list<int>({1, 2, 3, 4})) == "1..2..3..4" );
+    BOOST_REQUIRE( to_ip_string( make_tuple(1 , 2, 3, 4, 5 )) == "1..2..3..4..5" );
 }
 
 
